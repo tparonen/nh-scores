3a2eb33579dce0b5676e82eb8c6d7cf2d9af0f7c
Tatu Paronen <tatu.paronen@gmail.com>
2015-11-01 15:28:00 -0500

    Additional columns

--- app/js/modules/scores/ScoresTable.js
+++ app/js/modules/scores/ScoresTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Reflux from 'reflux'
+import moment from 'moment'
 import ScoresActions from './ScoresActions'
 import ScoresStore from './ScoresStore'
 
@@ -7,12 +8,34 @@ class ScoresRow extends React.Component {
   constructor(props) {
     super(props);
   }
+  getMoment(dateStr) {
+    return moment(dateStr, 'YYYYMMDD');
+  }
+  getDuration(start, end) {
+    return this.getMoment(end).diff(this.getMoment(start), 'days');
+  } 
+  formatDate(dateStr) {
+    return this.getMoment(dateStr).format('L');
+  }
+  formatCharacter(entry) {
+    return (
+      entry.role + '-' + entry.race + '-' +
+      entry.gender + '-' + entry.alignment
+    );
+  }
   render() {
+    var entry = this.props.entry;
     return (
       <tr>
-        <td>{this.props.entry.name}</td>
-        <td>{this.props.entry.score}</td>
-        <td>{this.props.entry.message}</td>
+        <td>{entry.name}</td>
+        <td>{this.formatCharacter(entry)}</td>
+        <td>{entry.score}</td>
+        <td>{entry.dungeon}</td>
+        <td>{entry.level}</td>
+        <td>{entry.hit_points}/{entry.max_hit_points}</td>
+        <td>{this.getDuration(entry.start_date, entry.end_date)} days</td>
+        <td>{this.formatDate(entry.end_date)}</td>
+        <td>{entry.message}</td>
       </tr>
     );
   }
@@ -46,8 +69,14 @@ export default class ScoresTable extends React.Component {
         <thead>
           <tr>
             <th>Name</th>
-            <th>Score</th>
-            <th>Message</th>
+            <th>Character</th>
+            <th>Points</th>
+            <th>Dungeon</th>
+            <th>Level</th>
+            <th>HP</th>
+            <th>Duration</th>
+            <th>Date</th>
+            <th>Reason</th>
           </tr>
         </thead>
         <tbody>
